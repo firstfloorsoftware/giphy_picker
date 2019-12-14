fd6ed45482fbc76327a7158eefaf61ff7804e5e1
Koen Zwikstra <kozw@firstfloorsoftware.com>
2019-12-14 22:21:48 +0100

    fixes full screen preview gif

--- lib/src/widgets/giphy_preview_page.dart
+++ lib/src/widgets/giphy_preview_page.dart
@@ -13,12 +13,20 @@ class GiphyPreviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
+
     return Scaffold(
         appBar: AppBar(title: title, actions: <Widget>[
           IconButton(icon: Icon(Icons.check), onPressed: () => onSelected(gif))
         ]),
         body: SafeArea(
-            child: Center(child: GiphyImage.original(gif: gif)),
+            child: Center(
+                child: GiphyImage.original(
+              gif: gif,
+              width: media.orientation == Orientation.portrait ? double.maxFinite : null,
+              height: media.orientation == Orientation.landscape ? double.maxFinite : null,
+              fit: BoxFit.contain,
+            )),
             bottom: false));
   }
 }
